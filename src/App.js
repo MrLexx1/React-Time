530860296e84e25c3a2aa9305ff765c48ebc732c
MrLexx1 <muhilman606@gmail.com>
2024-11-15 12:32:58 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState  /* eslint-disable-line */ } from "react";
 
 function App() {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimeOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {!timerOn && time === 0 && (
+          <button className="start" onClick={() => setTimeOn(true)}>
+            Start
+          </button>
+        )}
+        {timerOn && (
+          <button className="stop" onClick={() => setTimeOn(false)}>
+            Stop
+          </button>
+        )}
+        {!timerOn && time !== 0 && (
+          <button className="resume" onClick={() => setTimeOn(true)}>
+            Resume
+          </button>
+        )}
+        {!timerOn && time > 0 && (
+          <button className="reset" onClick={() => setTime(0)}>
+            Reset
+          </button>
+        )}
+      </div>
     </div>
   );
 }
